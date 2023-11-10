c4a1aac312a932cbd0ac6cf91c19a9f40832dd9b
Abdsamerhusari1 <abdalrahmansamerhusari@gmail.com>
2023-11-10 16:01:15 +0100

    nothing

--- test_connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'db_connect.php';
-// The connection logic in db_connect.php will execute here.
-// If the connection is successful, it will echo "Connected successfully".
-// If not, it will echo the error message.
-?>
