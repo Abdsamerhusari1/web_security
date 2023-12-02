5619791185e18dc957a6d9f986ac546725918159
emad <emad_aldeen.issawi.5771@student.lu.se>
2023-12-02 16:48:37 +0100

    htmlspecialchars()

--- login.php
+++ login.php
@@ -13,141 +13,215 @@ if (isset($error_message) && !empty($error_message)) {
 ?>
 	
 <title>Login</title>
-
 <?php
+/*HTTPOnly Cookies: HTTPOnly cookies cannot be accessed through JavaScript,
+which makes them more resilient to XSS attacks. This is set using the httponly flag in the setcookie function
+setcookie("name", "value", ["httponly" => true]);
+*/
+
+/*
+Set Cookie with Secure Flag: If your site is HTTPS only (which is recommended), ensure all cookies have the Secure flag set:
+setcookie("name", "value", ["secure" => true]);
+*/
+
+//Secure cookies are only sent over HTTPS connections.
+setcookie("name", "value", ["secure" => true, "httponly" => true, "samesite" => "strict"]);
+// Start or resume a session
 session_start();
+
+
+
+// Include the database connection script
 require_once('backend/db_connect.php'); // Adjust the path as needed
 
+// Define a constant to be used as a pepper in password hashing
 define('PEPPER', 'kQa9e4v8Jy3Cf1u5Rm7N0w2Hz8G6pX');
 
+// Initialize an empty error message string
 $errorMessage = "";
+// Set the maximum allowed login attempts
 $maxLoginAttempts = 3;
-$lockoutTime = 30 * 60; // 30 minutes in seconds
+// Define lockout time duration in seconds (30 minutes)
+$lockoutTime = 30 * 60;
 
 // Check if redirected from another page to login and store the redirection page
 $redirect = isset($_GET['from']) && $_GET['from'] === 'checkout' ? 'cart.php' : 'index.php';
 
+// Check if the form was submitted using POST method
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$username = trim($_POST["username"]);
-	$password = trim($_POST["password"]);
+    // Get and trim the username from the POST data
+    $username = trim(htmlspecialchars($_POST["username"]));
+    // Get and trim the password from the POST data
+    $password = trim(htmlspecialchars($_POST["password"]));
 
-	// User-specific lockout check
-	if (isset($_SESSION['login_attempts'][$username]) && $_SESSION['login_attempts'][$username]['count'] >= $maxLoginAttempts) {
-		$timeSinceLastAttempt = time() - $_SESSION['login_attempts'][$username]['last_attempt_time'];
-		if ($timeSinceLastAttempt < $lockoutTime) {
-			$errorMessage = "Too many failed login attempts. Please try again after 30 minutes.";
-		} else {
-			// Reset attempts if the lockout time has passed
-			$_SESSION['login_attempts'][$username]['count'] = 0;
-		}
-	}
+    // Check if the user has exceeded the maximum login attempts
+    if (isset($_SESSION['login_attempts'][$username]) && $_SESSION['login_attempts'][$username]['count'] >= $maxLoginAttempts) {
+        // Calculate the time since the last login attempt
+        $timeSinceLastAttempt = time() - $_SESSION['login_attempts'][$username]['last_attempt_time'];
+        // Check if the user is still within the lockout time
+        if ($timeSinceLastAttempt < $lockoutTime) {
+            // Set the error message for too many attempts
+            $errorMessage = "Too many failed login attempts. Please try again after 30 minutes.";
+        } else {
+            // Reset login attempts if the lockout time has passed
+            $_SESSION['login_attempts'][$username]['count'] = 0;
+        }
+    }
 
-	if (empty($errorMessage) && !empty($username) && !empty($password)) {
-		$stmt = $conn->prepare("SELECT user_id, password_hash FROM users WHERE username = ?");
-		$stmt->bind_param("s", $username);
-		$stmt->execute();
-		$stmt->store_result();
+    // Check if there are no errors and both username and password are provided
+    if (empty($errorMessage) && !empty($username) && !empty($password)) {
+        // Prepare a statement for user authentication
+        $stmt = $conn->prepare("SELECT user_id, password_hash FROM users WHERE username = ?");
+        // Bind the username parameter to the prepared statement
+        $stmt->bind_param("s", $username);
+        // Execute the prepared statement
+        $stmt->execute();
+        // Store the result to check the number of rows returned
+        $stmt->store_result();
 
-		if ($stmt->num_rows == 1) {
-			$stmt->bind_result($user_id, $password_hash);
-			$stmt->fetch();
+        // Check if exactly one user is found
+        if ($stmt->num_rows == 1) {
+            // Bind the result to variables
+            $stmt->bind_result($user_id, $password_hash);
+            // Fetch the result
+            $stmt->fetch();
 
-			// Verify the password with the pepper
-			if (password_verify($password . PEPPER, $password_hash)) {
-				// Password is correct
-				// Reset the login attempts for the user
-				unset($_SESSION['login_attempts'][$username]);
+            // Verify the password with the appended pepper
+            if (password_verify($password . PEPPER, $password_hash)) {
+                // Reset the login attempts for the user upon successful login
+                unset($_SESSION['login_attempts'][$username]);
 
-				// Start a new session
-				session_regenerate_id();
-				$_SESSION['loggedin'] = true;
-				$_SESSION['user_id'] = $user_id;
-				$_SESSION['username'] = $username;
+                // Regenerate the session ID
+                session_regenerate_id();
+                // Set session variables for logged-in status, user ID, and username
+                $_SESSION['loggedin'] = true;
+                $_SESSION['user_id'] = $user_id;
+                $_SESSION['username'] = $username;
 
-				// Redirect to the previous page if the 'from' query parameter is present and equals 'checkout'
-				if ($redirect === 'cart.php') {
-					header("Location: cart.php");
-					exit;
-				} else {
-					header("Location: index.php"); // Default redirect to home page
-					exit;
-				}
-				exit;
-			} else {
-				// Password is not correct
-				// Initialize or increment the login attempts counter
-				$_SESSION['login_attempts'][$username]['count'] = ($_SESSION['login_attempts'][$username]['count'] ?? 0) + 1;
-				$_SESSION['login_attempts'][$username]['last_attempt_time'] = time();
+                // Redirect to the previous page based on 'from' query parameter
+                if ($redirect === 'cart.php') {
+                    // Redirect to the cart page
+                    header("Location: cart.php");
+                    // Terminate the script
+                    exit;
+                } else {
+                    // Default redirect to the home page
+                    header("Location: index.php");
+                    // Terminate the script
+                    exit;
+                }
+                // Terminate the script (redundant, can be removed)
+                exit;
+            } else {
+                // Increment the login attempts counter for failed login
+                $_SESSION['login_attempts'][$username]['count'] = ($_SESSION['login_attempts'][$username]['count'] ?? 0) + 1;
+                $_SESSION['login_attempts'][$username]['last_attempt_time'] = time();
 
-				$attemptsLeft = $maxLoginAttempts - $_SESSION['login_attempts'][$username]['count'];
-				$errorMessage = "Invalid username or password. Attempts left: " . $attemptsLeft;
+                // Calculate the remaining attempts
+                $attemptsLeft = $maxLoginAttempts - $_SESSION['login_attempts'][$username]['count'];
+                // Set the error message for invalid login
+                $errorMessage = "Invalid username or password. Attempts left: " . $attemptsLeft;
 
-				// Check if the user should be locked out
-				if ($_SESSION['login_attempts'][$username]['count'] >= $maxLoginAttempts) {
-					$_SESSION['login_attempts'][$username]['last_attempt_time'] = time();
-					$errorMessage = "Too many failed login attempts. Please try again after 30 minutes.";
-				}
-			}
-		} else {
-			$errorMessage = "Invalid username or password.";
-		}
+                // Check if the user should be locked out
+                if ($_SESSION['login_attempts'][$username]['count'] >= $maxLoginAttempts) {
+                    // Set the time of the last attempt for lockout
+                    $_SESSION['login_attempts'][$username]['last_attempt_time'] = time();
+                    // Set the error message for too many attempts
+                    $errorMessage = "Too many failed login attempts. Please try again after 30 minutes.";
+                }
+            }
+        } else {
+            // Set the error message for invalid username or password
+            $errorMessage = "Invalid username or password.";
+        }
 
-		$stmt->close();
-	}
+        // Close the statement
+        $stmt->close();
+    }
 
-	$conn->close();
+    // Close the database connection
+    $conn->close();
 }
 ?>
 
 <!DOCTYPE html>
 <html class="h-full">
 <head>
+    <!-- Set character encoding for the webpage -->
     <meta charset="UTF-8">
+    <!-- Set the title of the web page -->
     <title>Login</title>
+    <!-- Include Tailwind CSS for styling -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
+
+
 <body class="flex flex-col min-h-screen bg-gray-100">
-    <!-- Navigation Bar -->
-    <nav class="bg-gray-800 p-4 text-white">
-        <div class="container mx-auto flex justify-between">
-            <div class="text-lg">Group 2 Shop</div>
-            <div>
-                <a href="index.php" class="px-3 hover:text-gray-300">Home</a>
-                <a href="cart.php" class="px-3 hover:text-gray-300">Cart</a>
-                <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
-                    <a href="logout.php" class="px-3 hover:text-gray-300">Logout</a>
-                <?php else: ?>
-                    <a href="login.php" class="px-3 hover:text-gray-300">Login</a>
-                <?php endif; ?>
-            </div>
+<!-- Navigation Bar -->
+<nav class="bg-gray-800 p-4 text-white">
+    <div class="container mx-auto flex justify-between">
+        <!-- Brand or company name -->
+        <div class="text-lg">Group 2 Shop</div>
+        <!-- Navigation links -->
+        <div>
+            <!-- Link to the home page -->
+            <a href="index.php" class="px-3 hover:text-gray-300">Home</a>
+            <!-- Link to the cart page -->
+            <a href="cart.php" class="px-3 hover:text-gray-300">Cart</a>
+            <!-- Conditional display of login/logout based on user session -->
+            <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true): ?>
+                <!-- Link to the logout page -->
+                <a href="logout.php" class="px-3 hover:text-gray-300">Logout</a>
+            <?php else: ?>
+                <!-- Link to the login page -->
+                <a href="login.php" class="px-3 hover:text-gray-300">Login</a>
+            <?php endif; ?>
         </div>
-    </nav>
+    </div>
+</nav>
 
-    <!-- Main Content Area -->
-	<div class="flex-grow container mx-auto px-4 mt-8">
-        <h1 class="text-2xl font-bold text-center">User Login</h1>
-        
-		<form action="login.php<?php echo !empty($_GET['from']) ? '?from=' . htmlspecialchars($_GET['from']) : ''; ?>" method="post" class="max-w-md mx-auto mt-4">
-            <div class="mb-4">
-                <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username:</label>
-                <input type="text" id="username" name="username" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
-            </div>
+<!-- Main Content Area -->
+<div class="flex-grow container mx-auto px-4 mt-8">
+    <!-- Page title -->
+    <h1 class="text-2xl font-bold text-center">User Login</h1>
 
-            <div class="mb-6">
-                <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
-                <input type="password" id="password" name="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
-                
-                <?php 
-                if (!empty($errorMessage)) {
-                    echo '<p class="text-red-500 text-sm">' . htmlspecialchars($errorMessage) . '</p>';
-                }
-                ?>
+    <!-- Login form -->
+    <form action="login.php<?php echo !empty($_GET['from']) ? '?from=' . htmlspecialchars($_GET['from']) : ''; ?>" method="post" class="max-w-md mx-auto mt-4">
+        <!-- Username input -->
+        <div class="mb-4">
+            <!-- Label for username -->
+            <label for="username" class="block text-gray-700 text-sm font-bold mb-2">Username:</label>
+            <!-- Text field for username -->
+            <input type="text" id="username" name="username" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+
+        </div>
+
+        <!-- Password input -->
+        <div class="mb-6">
+            <!-- Label for password -->
+            <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Password:</label>
+            <!-- Text field for password -->
+            <input type="password" id="password" name="password" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline">
+
+            <!-- Display error message if any -->
+            <?php
+            if (!empty($errorMessage)) {
+                echo '<p class="text-red-500 text-sm">' . htmlspecialchars($errorMessage) . '</p>';
+            }
+
+            ?>
             </div>
 
             <div class="flex items-center justify-between">
                 <input type="submit" value="Login" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
 				<a href="register.php" class="text-md bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Register</a>
 			</div>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            echo '<p class="text-gray-700 mt-2">Username Entered: ' . htmlspecialchars($username) . '</p>';
+        }
+        ?>
         </form>
     </div>
 
@@ -157,3 +231,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </footer>
 </body>
 </html>
+
+
+
+
--- register.php
+++ register.php
@@ -1,145 +1,165 @@
 <?php
+// Redirect to HTTPS if the request is not secure
 if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off') {
     $redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     header("Location: $redirectURL");
     exit;
-} 
+}
 ?>
 
 <?php
+// Display an error message if it is set
 if (isset($error_message) && !empty($error_message)) {
-	echo '<p style="color: red;">' . $error_message . '</p>';
+    echo '<p style="color: red;">' . $error_message . '</p>';
 }
 ?>
 <title>Register</title>
+
 <?php
-	session_start();
-	require_once('backend/db_connect.php');
+// Start the session
+session_start();
 
-	define('PEPPER', 'kQa9e4v8Jy3Cf1u5Rm7N0w2Hz8G6pX');
+// Include database connection script
+require_once('backend/db_connect.php');
 
-	// salting by PASSWORD_DEFAULT which also use (default hashing algorithm bcrypt) and pepper
-	function pepperedHash($password) {
-		return password_hash($password . PEPPER, PASSWORD_DEFAULT);
-	}
+// Define a constant for the pepper used in password hashing
+define('PEPPER', 'kQa9e4v8Jy3Cf1u5Rm7N0w2Hz8G6pX');
 
-	function isPasswordStrongEnough($password, $username, $address) {
-		// Minimum Length Check
-		if (strlen($password) < 8) {
-			return "Password must be at least 8 characters long.";
-		}
+// Function to hash the password with a pepper
+function pepperedHash($password) {
+    return password_hash($password . PEPPER, PASSWORD_DEFAULT);
+}
 
-		// Character Diversity Check
-		if (!preg_match('/[A-Z]/', $password) ||
-			!preg_match('/[a-z]/', $password) ||
-			!preg_match('/[0-9]/', $password) ||
-			!preg_match('/[\W]/', $password)) {
-			return "Password must include uppercase and lowercase letters, numbers, and special characters.";
-		}
+// Function to check if the password is strong enough
+function isPasswordStrongEnough($password, $username, $address) {
+    // Check for minimum length
+    if (strlen($password) < 8) {
+        return "Password must be at least 8 characters long.";
+    }
 
-		// Check for Personal Information
-		if (strpos($password, $username) !== false || strpos($password, $address) !== false) {
-			return "Password should not contain your username or address.";
-		}
+    // Check for character diversity
+    if (!preg_match('/[A-Z]/', $password) ||
+        !preg_match('/[a-z]/', $password) ||
+        !preg_match('/[0-9]/', $password) ||
+        !preg_match('/[\W]/', $password)) {
+        return "Password must include uppercase and lowercase letters, numbers, and special characters.";
+    }
 
-		// Sequential Characters Check
-		$length = strlen($password);
-		for ($i = 0; $i < $length - 2; $i++) {
-			// Numerical sequence
-			if (is_numeric($password[$i]) && 
-				is_numeric($password[$i + 1]) && 
-				is_numeric($password[$i + 2])) {
-				if (($password[$i + 1] == $password[$i] + 1) && 
-					($password[$i + 2] == $password[$i] + 2)) {
-					return "Password should not contain sequential numerical characters.";
-				}
-			}
+    // Check for personal information in password
+    if (strpos($password, $username) !== false || strpos($password, $address) !== false) {
+        return "Password should not contain your username or address.";
+    }
 
-			// Alphabetical sequence
-			if (ctype_alpha($password[$i]) && 
-				ctype_alpha($password[$i + 1]) && 
-				ctype_alpha($password[$i + 2])) {
-				if ((ord(strtolower($password[$i + 1])) == ord(strtolower($password[$i])) + 1) && 
-					(ord(strtolower($password[$i + 2])) == ord(strtolower($password[$i])) + 2)) {
-					return "Password should not contain sequential alphabetical characters.";
-				}
-			}
-		}
+    // Check for sequential characters
+    $length = strlen($password);
+    for ($i = 0; $i < $length - 2; $i++) {
+        // Numerical sequence check
+        if (is_numeric($password[$i]) &&
+            is_numeric($password[$i + 1]) &&
+            is_numeric($password[$i + 2])) {
+            if (($password[$i + 1] == $password[$i] + 1) &&
+                ($password[$i + 2] == $password[$i] + 2)) {
+                return "Password should not contain sequential numerical characters.";
+            }
+        }
 
-		// Repetitive Characters Check
-		for ($i = 0; $i < $length - 2; $i++) {
-			if ($password[$i] == $password[$i + 1] && $password[$i] == $password[$i + 2]) {
-				return "Password should not contain repetitive characters.";
-			}
-		}
+        // Alphabetical sequence check
+        if (ctype_alpha($password[$i]) &&
+            ctype_alpha($password[$i + 1]) &&
+            ctype_alpha($password[$i + 2])) {
+            if ((ord(strtolower($password[$i + 1])) == ord(strtolower($password[$i])) + 1) &&
+                (ord(strtolower($password[$i + 2])) == ord(strtolower($password[$i])) + 2)) {
+                return "Password should not contain sequential alphabetical characters.";
+            }
+        }
+    }
 
-		// Load password blacklist
-		$blacklist = file('blacklist/password-blacklist.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    // Check for repetitive characters
+    for ($i = 0; $i < $length - 2; $i++) {
+        if ($password[$i] == $password[$i + 1] && $password[$i] == $password[$i + 2]) {
+            return "Password should not contain repetitive characters.";
+        }
+    }
 
-		// Check if the password contains any word from the blacklist
-		foreach ($blacklist as $blacklistedWord) {
-			if (strpos($password, $blacklistedWord) !== false) {
-				return "The password cannot contain common words or patterns (e.g., 'password').";
-			}
-		}
+    // Load a blacklist of passwords
+    $blacklist = file('blacklist/password-blacklist.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
-		return true;
-	}
+    // Check if the password is in the blacklist
+    foreach ($blacklist as $blacklistedWord) {
+        if (strpos($password, $blacklistedWord) !== false) {
+            return "The password cannot contain common words or patterns (e.g., 'password').";
+        }
+    }
 
-	$errorMessage = "";
-	$successMessage = "";
+    // Return true if the password passes all checks
+    return true;
+}
 
-	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$username = trim($_POST["username"]);
-		$password = trim($_POST["password"]);
-		$address = trim($_POST["address"]);
+// Initialize error and success message variables
+$errorMessage = "";
+$successMessage = "";
 
-		if (empty($username) || empty($password) || empty($address)) {
-			$errorMessage = "Please fill in all fields.";
-		} else {
-			// Validate password strength
-			$passwordStrengthCheck = isPasswordStrongEnough($password, $username, $address);
-			if ($passwordStrengthCheck !== true) {
-				$errorMessage = $passwordStrengthCheck;
-			} else {
-				// Check if username already exists
-				$stmt = $conn->prepare("SELECT user_id FROM users WHERE username = ?");
-				$stmt->bind_param("s", $username);
-				$stmt->execute();
-				$stmt->store_result();
+// Check if the form was submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Capture and trim the user input
+    $username = trim(htmlspecialchars($_POST["username"]));
+    $password = trim(htmlspecialchars($_POST["password"]));
+    $address = trim(htmlspecialchars($_POST["address"]));
 
-				if ($stmt->num_rows > 0) {
-					$errorMessage = "Username already taken.";
-				} else {
-					// Hash the password with the pepper
-					$password_hash = pepperedHash($password);
+    // Check if all fields are filled
+    if (empty($username) || empty($password) || empty($address)) {
+        $errorMessage = "Please fill in all fields.";
+    } else {
+        // Check password strength
+        $passwordStrengthCheck = isPasswordStrongEnough($password, $username, $address);
+        if ($passwordStrengthCheck !== true) {
+            $errorMessage = $passwordStrengthCheck;
+        } else {
+            // Check if the username already exists
+            $stmt = $conn->prepare("SELECT user_id FROM users WHERE username = ?");
+            $stmt->bind_param("s", $username);
+            $stmt->execute();
+            $stmt->store_result();
 
-					$insert = $conn->prepare("INSERT INTO users (username, password_hash, address) VALUES (?, ?, ?)");
-					$insert->bind_param("sss", $username, $password_hash, $address);
-					
-					if ($insert->execute()) {
-						$_SESSION['loggedin'] = true;
-						$_SESSION['user_id'] = $conn->insert_id; // Get the new user's ID
-						$_SESSION['username'] = $username;
-						
-						// Store the success message in a session variable
-    					$_SESSION['successMessage'] = "You have registered successfully and are now logged in.";
-						// Redirect to the homepage
-						header("Location: index.php");
-						exit;
-					} else {
-						$errorMessage = "Error: " . $conn->error;
-					}
+            // If the username exists, show an error message
+            if ($stmt->num_rows > 0) {
+                $errorMessage = "Username already taken.";
+            } else {
+                // Hash the password
+                $password_hash = pepperedHash($password);
 
-					$insert->close();
-				}
+                // Insert the new user into the database
+                $insert = $conn->prepare("INSERT INTO users (username, password_hash, address) VALUES (?, ?, ?)");
+                $insert->bind_param("sss", $username, $password_hash, $address);
 
-				$stmt->close();
-			}
-		}
+                // Execute the insertion and check for success
+                if ($insert->execute()) {
+                    // Set session variables and redirect to the homepage
+                    $_SESSION['loggedin'] = true;
+                    $_SESSION['user_id'] = $conn->insert_id; // Get the new user's ID
+                    $_SESSION['username'] = $username;
 
-		$conn->close();
-	}
+                    // Store a success message in a session variable
+                    $_SESSION['successMessage'] = "You have registered successfully and are now logged in.";
+                    header("Location: index.php");
+                    exit;
+                } else {
+                    // Display an error message in case of a database error
+                    $errorMessage = "Error: " . $conn->error;
+                }
+
+                // Close the statement
+                $insert->close();
+            }
+
+            // Close the statement
+            $stmt->close();
+        }
+    }
+
+    // Close the database connection
+    $conn->close();
+}
 ?>
 
 <!DOCTYPE html>
@@ -196,6 +216,12 @@ if (isset($error_message) && !empty($error_message)) {
 						<input type="submit" value="Register" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
 						<a href="login.php" class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800">Already have an account?</a>
 					</div>
+                    <?php
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        echo '<p class="text-gray-700 mt-2">Username Entered: ' . htmlspecialchars($username) . '</p>';
+                        echo '<p class="text-gray-700">Home Address Entered: ' . htmlspecialchars($address) . '</p>';
+                    }
+                    ?>
 				</form>
 
 			<!-- Password Criteria -->
