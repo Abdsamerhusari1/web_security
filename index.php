4276a049f45206b674cfff64bb4f7f2ab3dc2aed
Abdsamerhusari1 <abdalrahmansamerhusari@gmail.com>
2023-12-14 08:48:04 +0100

    vun

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-if (empty($_SESSION['csrf_token'])) {
-    $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
-}
+// if (empty($_SESSION['csrf_token'])) {
+//     $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+// }
 require_once('backend/db_connect.php');
 
 // Check if the connection is not secure (HTTP) and redirect to HTTPS if needed.
@@ -27,11 +27,11 @@ function addToCart($productId, $quantity, $price) {
 
 // Check if the "Add to Cart" button was clicked.
 if (isset($_POST['add_to_cart'])) {
-    if (!isset($_POST['csrf_token'])) {
-        die('ERROR');
-    } else if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
-        die('Invalid CSRF token');
-    }
+    //if (!isset($_POST['csrf_token'])) {
+    //    die('ERROR');
+    //} else if ($_POST['csrf_token'] !== $_SESSION['csrf_token']) {
+    //    die('Invalid CSRF token');
+    //}
 
     $productId = $_POST['product_id'];
     $quantity = 1; 
@@ -109,7 +109,7 @@ $result = $conn->query($query);
                             <form method="post" action="index.php">
                                 <input type="hidden" name="product_id" value="<?php echo $row['product_id']; ?>">
                                 <input type="hidden" name="product_price" value="<?php echo $row['price']; ?>">
-                                <input type="hidden" name="csrf_token" value="<?php echo $_SESSION['csrf_token']; ?>">
+                                <!-- <input type="hidden" name="csrf_token" value="<?php //echo $_SESSION['csrf_token']; ?>"> -->
                                 <input type="submit" name="add_to_cart" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" value="Add to Cart">
                             </form>
                         <?php else: ?>
